4834b896bb7d0c048af32cb28271453939c8383e
chan <charles098@naver.com>
2021-05-13 10:40:05 +0900

    지뢰 박스 모든 기능 구현 완료

--- Minesweeper.js
+++ Minesweeper.js
@@ -1,24 +1,31 @@
 const cells = document.querySelectorAll('td');
 const box = document.querySelector('table');
 
-let rowNum = 6; // 박스 행 수
-let colNum = 6; // 박스 열 수
-let mineNum = 6; // 지뢰 개수
+let rowNum = 9; // 박스 행 수
+let colNum = 9; // 박스 열 수
+let mineNum = 10; // 지뢰 개수
 let mousedown = false; // 마우스 클릭 감지
+let focusColor = '#DBDBDB';
+let clickColor = '#EDF3F5';
+let BgColor = '#C3C4CD';
+
+
 
 // 6x6 빈 행렬, 방문 배열 생성
 const table = Array.from(Array(rowNum), () => Array(colNum).fill(0));
 const visit = Array.from(Array(rowNum), () => Array(colNum).fill(0)); // 0: non-visit, 1: visit, 2: flag
+const fix = Array.from(Array(rowNum), () => Array(colNum).fill(0));
 
 // 1. 지뢰 설치 및 데이터 반영
 setMine();
-load();
+console.log(fix);
+console.log(visit);
 
 // 2. 마우스 누르는 이벤트 추가
 // 깃발: 무반응. visit == 2
 // 빈칸: 빈칸만 hover. visit == 0
 // 빈칸 아님: 자기 테두리에 빈칸 아닌 칸들 hover. visit == 1
-box.addEventListener('mousedown', mouseDownHandler);
+document.addEventListener('mousedown', mouseDownHandler);
 box.addEventListener('mouseover', mouseOverHandler);
 
 // 3. 마우스 누른 상태에서 떼는 이벤트 추가
@@ -43,7 +50,7 @@ function mouseOverHandler(e){
     let col = e.target.dataCol;
     if(mousedown && e.target.tagName == 'TD'){
         // 방문 안한 경우 그 칸만 색 변화
-        if(visit[row][col] == 0) e.target.style.backgroundColor = 'green';
+        if(visit[row][col] == 0) e.target.style.backgroundColor = focusColor;
         
         // 방문한 경우 주변에 방문하지 않은 칸 모두 색 변화
         if(visit[row][col] == 1){
@@ -56,7 +63,7 @@ function mouseOutHandler(e){
     let row = e.target.dataRow;
     let col = e.target.dataCol;
     if(mousedown && e.target.tagName == 'TD'){
-        if(visit[row][col] == 0) e.target.style.backgroundColor = '#CBC1B7';
+        if(visit[row][col] == 0) e.target.style.backgroundColor = BgColor;
         
         // 방문한 경우 주변에 방문하지 않은 칸 모두 색 변화
         if(visit[row][col] == 1){
@@ -73,15 +80,17 @@ function mouseDownHandler(e){
     let col = e.target.dataCol;
     mousedown = true;
 
-    if(e.button == 0 || e.button == 2){
-        console.log('누름');
-        // 빈칸: 빈칸만 hover. visit == 0
-        // 빈칸 아님: 자기 테두리에 빈칸 아닌 칸들 hover. visit == 1
-        if(visit[row][col] == 0){
-            e.target.style.backgroundColor = 'green';
-        }
-        if(visit[row][col] == 1 && e.button != 2){
-            paintAround(row, col, 'on');
+    if(e.target.tagName == 'TD'){
+        if(e.button == 0 || e.button == 2){
+            console.log('누름');
+            // 빈칸: 빈칸만 hover. visit == 0
+            // 빈칸 아님: 자기 테두리에 빈칸 아닌 칸들 hover. visit == 1
+            if(visit[row][col] == 0){
+                e.target.style.backgroundColor = focusColor;
+            }
+            if(visit[row][col] == 1 && e.button != 2){
+                paintAround(row, col, 'on');
+            }
         }
     }
 }
@@ -93,8 +102,8 @@ function mouseUpHandler(e){
     
     if(e.target.tagName == 'TD'){
         // 클릭으로 인한 색 변경 취소
-        if(e.target.style.backgroundColor == 'green'){
-            e.target.style.backgroundColor = '#CBC1B7';
+        if(e.target.style.backgroundColor == focusColor){
+            e.target.style.backgroundColor = BgColor;
         }
 
         if(e.button == 2){
@@ -102,56 +111,66 @@ function mouseUpHandler(e){
             if(visit[row][col] == 2){
                 // 깃발 있으면 없앰
                 visit[row][col] = 0;
-                e.target.style.backgroundColor = '#CBC1B7';
+                e.target.style.backgroundImage = "url('')";
                 
-                if(table[row][col] == -1){
-                    // 지뢰가 맞으면 주변 숫자 초기화 - 1씩 더해줘야함
-                    updateAround(row, col, '+');
-                }
+    
+                updateAround(row, col, '+');
+                
             }
             else if(visit[row][col] == 0){
                 // 깃발 없으면 삽입
                 visit[row][col] = 2;
-                e.target.style.backgroundColor = '#E6C659';
+                pushVal(row, col, -2);
+                e.target.style.backgroundColor = BgColor;
 
-                if(table[row][col] == -1){
-                    // 지뢰가 맞으면 주변 숫자 초기화 - 1씩 빼줘야함
-                    updateAround(row, col,'-');
-                }
+                
+                updateAround(row, col,'-');
+                
             }
         }
         if(e.button == 0){
             console.log('좌 뗌');
             paintAround(row, col, 'off');
 
-            if(visit[row][col] == 0){
-                visit[row][col] = 1; // 방문처리
-                if(table[row][col] == -1){
+            if(visit[row][col] == 0){ 
+                visit[row][col] = 1;
+                if(fix[row][col] == -1){
                     // 지뢰 밟아서 게임끝
-                    e.target.style.backgroundColor = 'red';
-                    setTimeout(() => alert('Game over'), 10);
-                    // gameOver 함수 호출 -- 만들어야 함
+                    gameOver(row, col);
                 }
                 if(table[row][col] > 0){
                     // 값이 0이 아니면 그 칸만 색칠
-                    e.target.style.backgroundColor = '#E58366';
+                    pushVal(row, col, fix[row][col]);
                 }
                 if(table[row][col] == 0){
                     // 값이 0이면 bfs로 탐색해서 가능한 칸 모두 색칠
-                    // findAll(row, col) 구현
-                    e.target.style.backgroundColor = '#E58366';
+                    pushVal(row, col, fix[row][col]);
                     findAll(row, col);
                 }
             }
-            else if(visit[row][col] == 1){
+            else if(visit[row][col] == 1){ 
                 // 방문한 경우 - 인접한 칸 검사해서 가능한거 다 표시. 깃발 칸은 값을 0으로 본다
-                // findAll(row, col) 구현
                 findAll(row, col);
             }
         }
     }
 }
 
+
+function gameOver(row, col){
+    cells[rowNum * row + col].style.backgroundColor = '#E38985';
+    box.style.pointerEvents = 'none';
+    for(let i = 0; i < rowNum; i++){
+        for(let j = 0; j < colNum; j++){
+            if(fix[i][j] == -1 && visit[i][j] != 2) pushVal(i, j, -1);
+
+            // 지뢰가 아닌데 깃발 표시한거면 잘못. 표시해준다
+            if(visit[i][j] == 2 && fix[i][j] != -1) cells[rowNum * i + j].style.backgroundColor = '#F6D6CF';
+        }
+    }
+
+    setTimeout(() => alert('Game over'), 50);
+}
 
 function findAll(row, col){
     // (table[row][col] == 0)인 경우 
@@ -176,7 +195,7 @@ function findAll(row, col){
                 q.push([i,j]);
                 zero.push([i,j]);
                 visit[i][j] = 1;
-                cells[rowNum * i + j].style.backgroundColor = '#E58366';
+                pushVal(i, j, 0);
             }
         }
     }
@@ -199,15 +218,19 @@ function setMine(){
     let row, col, idx;
     for(let i = 0; i < mineNum; i++){ 
         idx = tmp.splice(Math.floor(Math.random() * tmp.length),1)[0]
-        row = parseInt(idx / 6);
-        col = idx % 6;
+        row = parseInt(idx / rowNum);
+        col = idx % rowNum;
         table[row][col] = -1;
+        fix[row][col] = -1;
     }
 
     // 나머지 숫자 초기화 + cells에 id 추가
     for(let i = 0; i < rowNum; i++){
         for(let j = 0; j < colNum; j++){
-            if(table[i][j] == 0) table[i][j] = myNum(i,j);
+            if(table[i][j] == 0){
+                table[i][j] = myNum(i,j);
+                fix[i][j] = myNum(i,j);
+            }
             cells[rowNum * i + j].dataRow = i;
             cells[rowNum * i + j].dataCol = j;
         }
@@ -217,43 +240,126 @@ function setMine(){
 
 // 마우스 눌렀을때 주변칸 색칠
 function paintAround(i, j, paint){
-    let color;
-    if(paint == 'on') color = 'green';
-    if(paint == 'off') color = '#CBC1B7';
-    if(paint == 'bfs') color = '#E58366';
+    let color, r, c;
+    let end = false;
+    if(paint == 'on') color = focusColor; 
+    if(paint == 'off') color = BgColor; 
+    if(paint == 'bfs') color = clickColor;
     
     if(isRange(i, j+1) && visit[i][j+1] == 0){
-        if(paint == 'bfs') visit[i][j+1] = 1;
-        cells[rowNum * i + (j+1)].style.backgroundColor = color; //동 : [row][col+1]
+        if(paint == 'bfs'){
+            if(fix[i][j+1] == -1){
+                end = true;
+                r = i;
+                c = j+1;
+            } 
+            else{
+                visit[i][j+1] = 1;
+                pushVal(i,j+1,fix[i][j+1]);
+            }
+        }
+        else cells[rowNum * i + (j+1)].style.backgroundColor = color; //동 : [row][col+1]
     }
     if(isRange(i-1, j+1) && visit[i-1][j+1] == 0){
-        if(paint == 'bfs') visit[i-1][j+1] = 1;
-        cells[rowNum * (i-1) + (j+1)].style.backgroundColor = color; // 동북 : [row-1][col+1]
+        if(paint == 'bfs'){
+            if(fix[i-1][j+1] == -1){
+                end = true;
+                r = i-1;
+                c = j+1;
+            }
+            else{
+            visit[i-1][j+1] = 1;
+            pushVal(i-1,j+1,fix[i-1][j+1]);
+            }
+        }
+        else cells[rowNum * (i-1) + (j+1)].style.backgroundColor = color; // 동북 : [row-1][col+1]
     }
     if(isRange(i-1, j) && visit[i-1][j] == 0){
-        if(paint == 'bfs') visit[i-1][j] = 1;
-        cells[rowNum * (i-1) + (j)].style.backgroundColor = color; // 북 : [row-1][col]
+        if(paint == 'bfs'){
+            if(fix[i-1][j] == -1){
+                end = true;
+                r = i-1;
+                c = j;
+            }
+            else{
+                visit[i-1][j] = 1;
+                pushVal(i-1,j,fix[i-1][j]);
+            }
+        }
+        else cells[rowNum * (i-1) + (j)].style.backgroundColor = color; // 북 : [row-1][col]
     }
     if(isRange(i-1, j-1) && visit[i-1][j-1] == 0){
-        if(paint == 'bfs') visit[i-1][j-1] = 1;
-        cells[rowNum * (i-1) + (j-1)].style.backgroundColor = color; // 북서 : [row-1][col-1]
+        if(paint == 'bfs'){
+            if(fix[i-1][j-1] == -1){
+                end = true;
+                r = i-1;
+                c = j-1;
+            }
+            else{ 
+                visit[i-1][j-1] = 1;
+                pushVal(i-1,j-1,fix[i-1][j-1]);
+            }
+        }
+        else cells[rowNum * (i-1) + (j-1)].style.backgroundColor = color; // 북서 : [row-1][col-1]
     }
     if(isRange(i, j-1) && visit[i][j-1] == 0){
-        if(paint == 'bfs') visit[i][j-1] = 1;
-        cells[rowNum * i + (j-1)].style.backgroundColor = color; // 서 : [row][col-1]
+        if(paint == 'bfs'){
+            if(fix[i][j-1] == -1){
+                end = true;
+                r = i;
+                c = j-1;
+            }
+            else{
+                visit[i][j-1] = 1;
+                pushVal(i,j-1,fix[i][j-1]);
+            }
+        }
+        else cells[rowNum * i + (j-1)].style.backgroundColor = color; // 서 : [row][col-1]
     }
     if(isRange(i+1, j-1) && visit[i+1][j-1] == 0){
-        if(paint == 'bfs') visit[i+1][j-1] = 1;
-        cells[rowNum * (i+1) + (j-1)].style.backgroundColor = color; // 남서 : [row+1][col-1]
+        if(paint == 'bfs'){
+            if(fix[i+1][j-1] == -1){
+                end = true;
+                r = i+1;
+                c = j-1;
+            }
+            else{
+                visit[i+1][j-1] = 1;
+                pushVal(i+1,j-1,fix[i+1][j-1]);
+            }
+        }
+        else cells[rowNum * (i+1) + (j-1)].style.backgroundColor = color; // 남서 : [row+1][col-1]
     }
     if(isRange(i+1, j) && visit[i+1][j] == 0){
-        if(paint == 'bfs') visit[i+1][j] = 1;
-        cells[rowNum * (i+1) + (j)].style.backgroundColor = color; // 남 : [row+1][col]
+        if(paint == 'bfs'){
+            if(fix[i+1][j] == -1){
+                end = true;
+                r = i+1;
+                c = j;
+            }
+            else{
+                visit[i+1][j] = 1;
+                pushVal(i+1,j,fix[i+1][j]);
+            }
+        }
+        else cells[rowNum * (i+1) + (j)].style.backgroundColor = color; // 남 : [row+1][col]
     }
     if(isRange(i+1, j+1) && visit[i+1][j+1] == 0){
-        if(paint == 'bfs') visit[i+1][j+1] = 1;
-        cells[rowNum * (i+1) + (j+1)].style.backgroundColor = color; // 동남 : [row+1][col+1]
+        if(paint == 'bfs'){
+            if(fix[i+1][j+1] == -1){
+                end = true;
+                r = i+1;
+                c = j+1;
+            }
+            else{
+                visit[i+1][j+1] = 1;
+                pushVal(i+1,j+1,fix[i+1][j+1]);
+            }
+        }
+        else cells[rowNum * (i+1) + (j+1)].style.backgroundColor = color; // 동남 : [row+1][col+1]
     }
+
+    if(end) gameOver(r,c);
 }
 
 
@@ -279,7 +385,6 @@ function updateAround(row, col, how){
         if(isRange(row+1, col) && table[row+1][col] != -1) table[row+1][col] += 1; // 남 : [row+1][col]
         if(isRange(row+1, col+1) && table[row+1][col+1] != -1) table[row+1][col+1] += 1; // 동남 : [row+1][col+1]
     }
-    load();
 }
 
 
@@ -317,12 +422,50 @@ function isRange(row, col){
     return false;
 }
 
+function pushVal(row, col, number){
+    let elem = cells[rowNum * row + col];
 
-// 데이터 반영
-function load(){
-    for(let i = 0; i < rowNum; i++){
-        for(let j = 0; j < colNum; j++){
-            cells[rowNum * i + j].textContent = table[i][j];
-        }
+    switch(number){
+        case -2: // 깃발 - 사진
+            elem.style.backgroundImage = "url('./images/flag.png')";
+            break;
+        case -1: // 지뢰 - 사진
+            elem.style.backgroundImage = "url('./images/bomb.png')";
+            break;
+        case 0:
+            elem.style.backgroundColor = clickColor;
+            break;
+        case 1:
+            elem.style.backgroundImage = "url('./images/one.png')";
+            elem.style.backgroundColor = clickColor;
+            break;
+        case 2:
+            elem.style.backgroundImage = "url('./images/two.png')";
+            elem.style.backgroundColor = clickColor;
+            break;
+        case 3:
+            elem.style.backgroundImage = "url('./images/three.png')";
+            elem.style.backgroundColor = clickColor;
+            break;
+        case 4:
+            elem.style.backgroundImage = "url('./images/four.png')";
+            elem.style.backgroundColor = clickColor;
+            break;
+        case 5:
+            elem.style.backgroundImage = "url('./images/five.png')";
+            elem.style.backgroundColor = clickColor;
+            break;
+        case 6:
+            elem.style.backgroundImage = "url('./images/six.png')";
+            elem.style.backgroundColor = clickColor;
+            break;
+        case 7:
+            elem.style.backgroundImage = "url('./images/seven.png')";
+            elem.style.backgroundColor = clickColor;
+            break;
+        case 8:
+            elem.style.backgroundImage = "url('./images/eight.png')";
+            elem.style.backgroundColor = clickColor;
+            break;
     }
 }
